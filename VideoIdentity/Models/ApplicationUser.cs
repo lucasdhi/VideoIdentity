7e746c2eb2e972d99c5db7f54f4b7027e853374c
Lucas Soares <lucas28soares@hotmail.com.br>
2022-03-16 10:24:56 -0300

    Adicionar arquivos de projeto.

---
+++ VideoIdentity/Models/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VideoIdentity.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string CPF { get; set; }
+        public string endereço { get; set; }
+    }
+}
